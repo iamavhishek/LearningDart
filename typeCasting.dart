2bd8d949defe56364b3df926ad1c856444b23616
Abhishek Dahal <abhishek.bca76@hsm.edu.np>
2023-02-26 20:59:03 +0545

    Learned about type casting in dart

--- typeCasting.dart
+++ typeCasting.dart
@@ -8,16 +8,26 @@ void main() {
   print(num.toDouble());
   print(num.toDouble().runtimeType);
 
-  // Int to String
+  // Int to string
   print(num.toString());
   print(num.toString().runtimeType);
 
   print("\n---------------\n");
 
-  // double
+  // Double
   double num2 = 403.543;
   print(num2);
   print(num2.runtimeType);
 
-  // String
+  // Double to string
+  print(num2.toString());
+  print(num2.toStringAsFixed(
+      2)); // controls how many characters to show after decimal
+  print(num2.toString().runtimeType);
+
+  // Double to int
+  print(num2.toInt());
+  print(num2.toInt().runtimeType);
+
+  print("\n---------------\n");
 }
