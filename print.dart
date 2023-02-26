d8e9501279bc18d084434eccf810efd1f89b621d
Abhishek Dahal <abhishek.bca76@hsm.edu.np>
2023-02-27 01:34:31 +0545

    Learned about print statements in dart

--- print.dart
+++ print.dart
@@ -3,4 +3,15 @@ import 'dart:io';
 void main() {
   print("Hello World");
   stdout.write("Hello World 2\n");
+
+// This type of multiline print statement will preserve line breaks and white spaces
+  print("""This is a multi line
+  print statement used
+  by dart programming language
+  """);
+
+// This type of multiline print statement will not preserve line breaks and white spaces
+  print('This is also multi line '
+      'print statement used'
+      'by dart programming language');
 }
