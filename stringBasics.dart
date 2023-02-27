bdf1ac73182919d0a5fe6ed2b46545a791c1ee27
Abhishek Dahal <abhishek.bca76@hsm.edu.np>
2023-02-27 20:23:01 +0545

    Some string functions

--- stringBasics.dart
+++ stringBasics.dart
@@ -9,4 +9,15 @@ void main() {
   print(intro.replaceAll("Abhishek", "Ashish"));
   print(intro.trim()[0].codeUnits);
   print(intro.trim().codeUnits);
+
+// Output
+//    HI I'M ABHISHEK DAHAL.
+//    hi i'm abhishek dahal.
+//Hi I'm Abhishek Dahal.
+//[H, i,  , I, ', m,  , A, b, h, i, s, h, e, k,  , D, a, h, a, l, .]
+//H-i- -I-'-m- -A-b-h-i-s-h-e-k- -D-a-h-a-l-.
+//Sring is 22 character long
+//     Hi I'm Ashish Dahal.
+//[72]
+//[72, 105, 32, 73, 39, 109, 32, 65, 98, 104, 105, 115, 104, 101, 107, 32, 68, 97, 104, 97, 108, 46]
 }
