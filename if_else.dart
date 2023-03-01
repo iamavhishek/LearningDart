ad3b705f2b6a471660952d44819cacbd9822c0c5
Abhishek Dahal <abhishek.bca76@hsm.edu.np>
2023-03-01 20:39:08 +0545

    If Else Statement

--- if_else.dart
+++ if_else.dart
@@ -5,7 +5,7 @@ void main() {
   stdout.write("Enter your age: ");
   age = int.parse(stdin.readLineSync()!);
   if (age < 18) {
-    stdout.writeln("You are not permitted to drink");
+    stdout.writeln("You are under age so you are not permitted to drink");
   } else {
     stdout.writeln("You can drink");
   }
